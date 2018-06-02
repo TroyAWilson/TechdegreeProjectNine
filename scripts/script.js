e5287ff7f1f65fafe6cdcb52cc94c1f5ab5072f2
TroyAWilson <38123313+TroyAWilson@users.noreply.github.com>
2018-06-02 12:14:20 -0700

    Drop down menu complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
+const dropDown = document.getElementsByClassName('dropDown');
+const bell = document.getElementById('bell');
+const circle = document.getElementsByClassName('circle');
 
+bell.addEventListener("click", ()=>{
+  dropDown[0].classList.toggle("show");
+  circle[0].style.opacity ="0";
+});
